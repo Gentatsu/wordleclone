b1fb906e6f2a701d9f8c7b7e2dfcf7db4dec55c4
Naval Bhandari <navs92@hotmail.com>
2022-02-04 02:43:23 +0000

    Tidy up ternary for keyboard

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,21 +1,6 @@
 import React  from "react"
 import "./Key.css"
-import Cell from "./Cell"
-
-// export class Key extends React.Component {
-
-
-//   render() {
-//       const cellComponent = <div className="key" >
-//       <div className={classNames({
-//         keyletter: true,
-//         [this.state.keyState]: true
-//       })}>
-//         {this.state.letter}</div>
-//       </div>
-//       return cellComponent
-//   }
-// }
+import Cell, {unused, rightplace, wrongplace, wrongletter_keyboard} from "./Cell"
 
 export class Keyboard extends React.Component {
 
@@ -38,25 +23,13 @@ export class Keyboard extends React.Component {
       var corrects = attempt.map(function(letter) {
         return correctWord.indexOf(letter)
       }, this);
-      // console.log(corrects)
       for (let i = 0; i < attempt.length; i++) 
       { 
         var keyfunc = this.state.rows.find(key => key.props.letter === attempt[i]).ref.current;
         if (corrects[i] !== -1)
-        {
-          if (attempt[i] === correctWord[i])
-          {
-            keyfunc.updateState("rightplace")
-          }
-          else 
-          {
-            keyfunc.updateState("wrongplace")
-          }
-        }
+          keyfunc.updateState(attempt[i] === correctWord[i] ? rightplace: wrongplace)
         else
-        {
-          keyfunc.updateState("wrongletter_keyboard")
-        }
+          keyfunc.updateState(wrongletter_keyboard)
       }
     }
   }
